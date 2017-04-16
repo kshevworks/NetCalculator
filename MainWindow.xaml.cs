2f0d27d5ef7d0960d062469e0b139c5cf7aac0c8
Konstantin <kshevworks@gmail.com>
2017-04-16 18:55:03 +1000

    first commit
    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NetCalculator
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+
+
+        //Переводим из десятичной в двоичную
+        string decToBin (string input)
+        {
+            string output = "";
+            if ("" == input) return "";
+            int x = int.Parse(input);
+            while (x > 0)
+            {
+                output = x % 2 + output;
+                x /= 2;
+            }
+            if ("" == output)
+            {
+                output = "0";
+            }
+            if (output.Length<8)
+            {
+                int l = output.Length;
+                for(int i = 0; i < 8-l; i++)
+                {
+                    output = "0" + output;
+                }
+            }
+            return output;
+        }
+
+        //Переводим из двоичной в десятичную
+        string binToDec(string input)
+        {
+            string output = "";
+            int temp = 0;
+            for(int i = 0; i<input.Length; i++)
+            {
+                temp += int.Parse(output[i].ToString()) * (2 ^ i);
+            }
+            
+            return temp.ToString();
+        }
+
+        string decimalCut(string input)
+        {
+                try
+                {
+                    if (int.Parse(input) > 255)
+                        return "255";
+                }
+                catch
+                {
+                    return "";
+                }
+            return input;
+        }
+
+        string binaryCut(string input)
+        {
+            string output = "";
+            for (int i = 0; i < 8; i++)
+            {
+                output += input[i];
+            }
+            return output;
+        }
+
+
+        void setSelection(TextBox tb)
+        {
+            tb.Select(0, tb.Text.Length);
+        }
+
+
+
+        //Секция десятичного вида IP адреса
+        private void ipv4_dec_1oct_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            setSelection(ipv4_dec_1oct);
+        }
+
+        private void ipv4_dec_1oct_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            setSelection(ipv4_dec_1oct);
+        }
+
+        private void ipv4_dec_1oct_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ipv4_dec_1oct.Text = decimalCut(ipv4_dec_1oct.Text);
+            ipv4_dec_1oct.SelectionStart = ipv4_dec_1oct.Text.Length;
+
+            if (null != ipv4_bin_1oct)
+            {
+                ipv4_bin_1oct.Text = decToBin(ipv4_dec_1oct.Text);
+            }
+        }
+
+        private void ipv4_dec_2oct_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            setSelection(ipv4_dec_2oct);
+        }
+
+        private void ipv4_dec_2oct_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            setSelection(ipv4_dec_2oct);
+        }
+        private void ipv4_dec_2oct_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ipv4_dec_2oct.Text = decimalCut(ipv4_dec_2oct.Text);
+            ipv4_dec_2oct.SelectionStart = ipv4_dec_2oct.Text.Length;
+
+            if (null != ipv4_bin_2oct)
+            {
+                ipv4_bin_2oct.Text = decToBin(ipv4_dec_2oct.Text);
+            }
+        }
+        private void ipv4_dec_3oct_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            setSelection(ipv4_dec_3oct);
+        }
+
+        private void ipv4_dec_3oct_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            setSelection(ipv4_dec_3oct);
+        }
+        private void ipv4_dec_3oct_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ipv4_dec_3oct.Text = decimalCut(ipv4_dec_3oct.Text);
+            ipv4_dec_3oct.SelectionStart = ipv4_dec_3oct.Text.Length;
+
+            if (null != ipv4_bin_3oct)
+            {
+                ipv4_bin_3oct.Text = decToBin(ipv4_dec_3oct.Text);
+            }
+        }
+        private void ipv4_dec_4oct_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            setSelection(ipv4_dec_4oct);
+        }
+
+        private void ipv4_dec_4oct_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            setSelection(ipv4_dec_4oct);
+        }
+        private void ipv4_dec_4oct_GotFocus(object sender, RoutedEventArgs e)
+        {
+            setSelection(ipv4_dec_4oct);
+        }
+        private void ipv4_dec_4oct_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ipv4_dec_4oct.Text = decimalCut(ipv4_dec_4oct.Text);
+            ipv4_dec_4oct.SelectionStart = ipv4_dec_4oct.Text.Length;
+
+            if (null != ipv4_bin_4oct)
+            {
+                ipv4_bin_4oct.Text = decToBin(ipv4_dec_4oct.Text);
+            }
+        }
+
+
+        private void mask_dec_1oct_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            setSelection(mask_dec_1oct);
+        }
+
+        private void mask_dec_1oct_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            setSelection(mask_dec_1oct);
+        }
+
+        private void mask_dec_1oct_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            mask_dec_1oct.Text = decimalCut(mask_dec_1oct.Text);
+            mask_dec_1oct.SelectionStart = mask_dec_1oct.Text.Length;
+
+            if (null != mask_bin_1oct)
+            {
+                mask_bin_1oct.Text = decToBin(mask_dec_1oct.Text);
+            }
+        }
+
+        private void mask_dec_2oct_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            setSelection(mask_dec_2oct);
+        }
+
+        private void mask_dec_2oct_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            setSelection(mask_dec_2oct);
+        }
+        private void mask_dec_2oct_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            mask_dec_2oct.Text = decimalCut(mask_dec_2oct.Text);
+            mask_dec_2oct.SelectionStart = mask_dec_2oct.Text.Length;
+
+            if (null != mask_bin_2oct)
+            {
+                mask_bin_2oct.Text = decToBin(mask_dec_2oct.Text);
+            }
+        }
+        private void mask_dec_3oct_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            setSelection(mask_dec_3oct);
+        }
+
+        private void mask_dec_3oct_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            setSelection(mask_dec_3oct);
+        }
+        private void mask_dec_3oct_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            mask_dec_3oct.Text = decimalCut(mask_dec_3oct.Text);
+            mask_dec_3oct.SelectionStart = mask_dec_3oct.Text.Length;
+
+            if (null != mask_bin_3oct)
+            {
+                mask_bin_3oct.Text = decToBin(mask_dec_3oct.Text);
+            }
+        }
+        private void mask_dec_4oct_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            setSelection(mask_dec_4oct);
+        }
+
+        private void mask_dec_4oct_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            setSelection(mask_dec_4oct);
+        }
+        private void mask_dec_4oct_GotFocus(object sender, RoutedEventArgs e)
+        {
+            setSelection(mask_dec_4oct);
+        }
+        private void mask_dec_4oct_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            mask_dec_4oct.Text = decimalCut(mask_dec_4oct.Text);
+            mask_dec_4oct.SelectionStart = mask_dec_4oct.Text.Length;
+
+            if (null != mask_bin_4oct)
+            {
+                mask_bin_4oct.Text = decToBin(mask_dec_4oct.Text);
+            }
+        }
+
+
+
+    }
+}
